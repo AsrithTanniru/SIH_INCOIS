39603ea7a9b0790d0db16d0ebdaa130881904d84
Asrith Tanniru <asrithtanniru155@gmail.com>
2024-10-08 20:35:47 +0530

    Updated the dependencies and assests

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,6 @@
-// firebaseConfig.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
-import { initializeApp } from "firebase/app";
-
-//Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBD9hkkL7UwlQJczz13lweuxwg66TxCGs4",
   authDomain: "shore-alert-f0a9b.firebaseapp.com",
@@ -12,7 +10,8 @@ const firebaseConfig = {
   appId: "1:238621949666:web:d995adc47214fc19fea8bb"
 };
 
-
 const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
 
-export { app };
+export { firestore };
+console.log('Firestore initialized:', firestore);
